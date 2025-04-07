db23554d469da8146bd34e73b89abd16b1eb2515
Peter Marinov <peter@endatix.com>
2025-04-07 14:00:20 +0300

    Adding unknown answer viewer that renders JSON for questions that are not supported

---
+++ features/submissions/ui/answers/unknown-answer.tsx
@@ -0,0 +1,25 @@
+import { QuestionLabel } from "../details/question-label";
+import { ViewAnswerProps } from "./answer-viewer";
+
+const UnknownAnswerViewer = ({ forQuestion }: ViewAnswerProps) => {
+  if (forQuestion.getType() === "html" || forQuestion.getType() === "image") {
+    return null;
+  }
+
+  const isStringValue = typeof forQuestion?.value === "string";
+
+  return (
+    <>
+      <QuestionLabel forQuestion={forQuestion} />
+      {isStringValue ? (
+        <p className="col-span-3">{forQuestion.value}</p>
+      ) : (
+        <pre className="col-span-3 text-muted-foreground text-sm">
+          {JSON.stringify(forQuestion, null, 2)}
+        </pre>
+      )}
+    </>
+  );
+};
+
+export default UnknownAnswerViewer;
