97b8e627ee1b5f83a1f3ad625cf9101f994b016b
Dobromir Petrov <14013810+dspetrov@users.noreply.github.com>
2025-07-09 15:28:22 +0300

    fix: Nav bar is not sticky and Header is trsansperant

--- components/layout-ui/navigation/main-nav.tsx
+++ components/layout-ui/navigation/main-nav.tsx
@@ -22,30 +22,32 @@ const MainNav = () => {
 
   return (
     <TooltipProvider>
-      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5 space-y-1">
-        <NavLink
-          path={logo.path}
-          text={logo.text}
-          setIsActive={false}
-          className="group flex h-10 w-10 shrink-0 items-center justify-center rounded-sm gap-2 bg-primary text-lg font-semibold text-primary-foreground md:h-9 md:w-9 md:text-base"
-        >
-          <Image
-            aria-hidden
-            className="h-9 w-9 transition-all group-hover:scale-110 rounded-sm"
-            src={EndatixLogoSvg}
-            alt="logo"
-          />
-        </NavLink>
-        <NavItemsWithDialog navItems={navItemsWithIcons} />
-      </nav>
-      <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-        <NavLink 
-          path={settingsNavItem.path} 
-          text={settingsNavItem.text}
-        >
-          <settingsNavItem.IconType className="h-6 w-6" />
-        </NavLink>
-      </nav>
+      <div className="sticky top-0 h-screen flex flex-col">
+        <nav className="flex flex-col items-center gap-4 px-2 sm:py-5 space-y-1">
+          <NavLink
+            path={logo.path}
+            text={logo.text}
+            setIsActive={false}
+            className="group flex h-10 w-10 shrink-0 items-center justify-center rounded-sm gap-2 bg-primary text-lg font-semibold text-primary-foreground md:h-9 md:w-9 md:text-base"
+          >
+            <Image
+              aria-hidden
+              className="h-9 w-9 transition-all group-hover:scale-110 rounded-sm"
+              src={EndatixLogoSvg}
+              alt="logo"
+            />
+          </NavLink>
+          <NavItemsWithDialog navItems={navItemsWithIcons} />
+        </nav>
+        <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+          <NavLink 
+            path={settingsNavItem.path} 
+            text={settingsNavItem.text}
+          >
+            <settingsNavItem.IconType className="h-6 w-6" />
+          </NavLink>
+        </nav>
+      </div>
     </TooltipProvider>
   );
 };
