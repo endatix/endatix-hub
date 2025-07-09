a1ad78c6373b9dce7c6b22ae2fb1d3b495b86d95
Dobromir Petrov <14013810+dspetrov@users.noreply.github.com>
2025-07-09 16:21:20 +0300

    fix: Nav bar is not sticky and Header is trsansperant (#88)

--- app/(main)/@nav/[...catchAll]/page.tsx
+++ app/(main)/@nav/[...catchAll]/page.tsx
@@ -2,7 +2,7 @@ import MainNav from "@/components/layout-ui/navigation/main-nav";
 
 export default function NavSlot() {
   return (
-    <aside className="w-14 flex flex-col border-r bg-background h-screen">
+    <aside className="w-14 flex flex-col border-r bg-background">
       <MainNav />
     </aside>
   );
