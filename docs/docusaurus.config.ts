8587317e192eece1151645228e5691c4db80f965
Peter Marinov <peter@endatix.com>
2025-08-07 11:27:30 +0300

    Ensuring site is not indexed

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
+  noIndex: true,
   title: "Endatix Hub Documentation",
   tagline: "Official documentation for Endatix Hub",
   favicon: "img/endatix.svg",
