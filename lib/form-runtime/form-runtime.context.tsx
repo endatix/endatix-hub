981931d8606e1bea67ce416e84ea6dea25502602
Peter Marinov <peter@endatix.com>
2026-05-04 16:36:34 +0300

    Fix reported code quality issue

--- lib/form-runtime/form-runtime.context.tsx
+++ lib/form-runtime/form-runtime.context.tsx
@@ -67,7 +67,7 @@ export function FormRuntimeProvider({
 
   useEffect(() => {
     startTransition(() => {
-      void ensureFormAccessJwt();
+      ensureFormAccessJwt();
     });
   }, [ensureFormAccessJwt]);
 
