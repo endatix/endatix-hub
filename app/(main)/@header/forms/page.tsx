f025bfa7d8ae9cc6fe31eaece7b14bc03463c572
Peter Marinov <peter@endatix.com>
2026-05-21 16:21:06 +0300

    Fixing import issue

--- app/(main)/@header/forms/page.tsx
+++ app/(main)/@header/forms/page.tsx
@@ -2,8 +2,10 @@ import { auth } from "@/auth";
 import MainHeader from "@/components/layout-ui/header/main-header";
 import FormsBreadcrumbNav from "@/components/layout-ui/navigation/forms-breadcrumb-nav";
 import { Skeleton } from "@/components/ui/skeleton";
-import { buildFormsBreadcrumbModel } from "@/features/folders/view-forms-header";
-import { getFormsHeaderDataCached } from "@/features/folders/view-forms-header";
+import {
+  getFormsHeaderDataCached,
+  buildFormsBreadcrumbModel,
+} from "@/features/folders/view-forms-header";
 import { AssetStorageProvider } from "@/features/asset-storage/server";
 import CreateFormSheet from "@/features/forms/ui/create-form-sheet";
 import { FormAssistantProvider } from "@/features/forms/use-cases/design-form/form-assistant.context";
