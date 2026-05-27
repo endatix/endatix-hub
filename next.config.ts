ef57afb6095257d0c99acbf0582788f2ce04a06f
Peter Marinov <peter@endatix.com>
2026-05-27 15:21:30 +0300

    Add basePath config to support Next.js deployment scenarios deployment under subfolder

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ import { withEndatix } from "@/features/config";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone", // Used to decrease the size of the application, check https://nextjs.org/docs/pages/api-reference/next-config-js/output
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   typedRoutes: true,
   reactStrictMode: true,
   reactCompiler: true,
